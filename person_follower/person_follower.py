731c59903c9656c49b46a0137a18ae6e8dc2ca2f
Enric Cervera <ecervera@uji.es>
2023-12-18 12:45:24 +0100

    Publish twist

--- person_follower/person_follower.py
+++ person_follower/person_follower.py
@@ -16,13 +16,13 @@ import rclpy
 from rclpy.node import Node
 
 from sensor_msgs.msg import LaserScan
-from std_msgs.msg import String
+from geometry_msgs.msg import Twist
 
 class PersonFollower(Node):
 
     def __init__(self):
         super().__init__('person_follower')
-        self.publisher_ = self.create_publisher(String, 'topic', 10)
+        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
         self.subscription = self.create_subscription(
             LaserScan,
             '/scan',
@@ -31,19 +31,25 @@ class PersonFollower(Node):
         self.subscription  # prevent unused variable warning
 
     def listener_callback(self, input_msg):
-        self.get_logger().info("I heard a scan")
-        output_msg = String()
-        output_msg.data = 'Hello World'
+        angle_min = input_msg.angle_min
+        angle_max = input_msg.angle_max
+        angle_increment = input_msg.angle_increment
+        ranges = input_msg.ranges
+        #
+        # your code for computing vx, wz
+        #
+        vx = 0.
+        wz = 0.
+        #
+        output_msg = Twist()
+        output_msg.linear.x = vx
+        output_msg.angular.z = wz
         self.publisher_.publish(output_msg)
-        self.get_logger().info('Publishing: "%s"' % output_msg.data)
 
 def main(args=None):
     rclpy.init(args=args)
-
     person_follower = PersonFollower()
-
     rclpy.spin(person_follower)
-
     person_follower.destroy_node()
     rclpy.shutdown()
 
