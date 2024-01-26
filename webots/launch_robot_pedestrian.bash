3c9bdf42cc74d0100ec077e664ae71bd861c149a
Enric Cervera <ecervera@uji.es>
2024-01-26 13:31:38 +0100

    Update path

--- webots/launch_robot_pedestrian.bash
+++ webots/launch_robot_pedestrian.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export WEBOTS_HOME=/home/ecervera/webots_R2023a
+export WEBOTS_HOME=$HOME/webots_R2023a
 source /opt/ros/foxy/setup.bash
 
 ros2 launch webots_ros2_turtlebot robot_launch.py \
